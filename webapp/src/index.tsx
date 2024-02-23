1c10caf5451c5ddc57b3c3daaf3cf95540374931
Joram Wilander <jwawilander@gmail.com>
2024-02-23 14:10:13 -0500

    Add post dropdown to add properties

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -5,8 +5,11 @@ import {GlobalState} from '@mattermost/types/lib/store';
 import {manifest} from 'src/manifest';
 import {PluginRegistry} from 'src/types/mattermost-webapp';
 import reducer from 'src/reducer';
-import {displayManageFieldsModal} from 'src/actions';
+import {displayManageFieldsModal, receivedProperty} from 'src/actions';
 import PostAttachment from 'src/components/post_attachment';
+import {createProperty, fetchPropertyFieldsForTerm} from 'src/client';
+import {Property, PropertyField} from 'src/types/property';
+import {ReceivedProperty} from 'src/types/actions';
 
 export default class Plugin {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
@@ -14,6 +17,36 @@ export default class Plugin {
         registry.registerReducer(reducer);
         registry.registerPostMessageAttachmentComponent(PostAttachment);
         registry.registerMainMenuAction('Manage Fields', () => store.dispatch(displayManageFieldsModal({})));
+        const {rootRegisterMenuItem} = registry.registerPostDropdownSubMenuAction('Add Property');
+
+        const fields = await fetchPropertyFieldsForTerm('');
+
+        const makeCreateProperty = (field: PropertyField) => {
+            return async (postID: string) => {
+                let defaultValue = [] as string[];
+                if (field.type === 'select') {
+                    defaultValue = field?.values ? [field.values[0]] : [];
+                }
+                const {id} = await createProperty(postID, 'post', field.id, defaultValue);
+                store.dispatch<ReceivedProperty>(receivedProperty({
+                    id,
+                    object_id: postID,
+                    object_type: 'post',
+                    property_field_id: field.id,
+                    property_field_name: field.name,
+                    property_field_type: field.type,
+                    property_field_values: field.values,
+                    value: defaultValue,
+                } as Property));
+            };
+        };
+
+        fields.forEach((field) => {
+            rootRegisterMenuItem(
+                field.name,
+                makeCreateProperty(field),
+            );
+        });
     }
 }
 
--- webapp/src/types/mattermost-webapp/index.d.ts
+++ webapp/src/types/mattermost-webapp/index.d.ts
@@ -1,7 +1,11 @@
+
+type PostMenuAction = (postID: string) => void;
+type PostMenuFilter = (postID: string) => boolean;
 export interface PluginRegistry {
     registerReducer(reducer: any)
-    registerPostMessageAttachmentComponent (component: React.ElementType)
+    registerPostMessageAttachmentComponent(component: React.ElementType)
     registerMainMenuAction(text: string, action: () => void, mobileIcon?: React.ElementType)
+    registerPostDropdownSubMenuAction(text: string, action?: PostMenuAction, filter?: PostMenuFilter) : {id: string, rootRegisterMenuItem: (innerText: string, innerAction: PostMenuAction, innerFilter?: PostMenuFilter) => void}
 
     // Add more if needed from https://developers.mattermost.com/extend/plugins/webapp/reference
 }
